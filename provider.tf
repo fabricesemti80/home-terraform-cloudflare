e81c185a1b749440129aa91198e08491d9e56be8
Fabrice Semti <fabrice@fabricesemti.com>
2025-01-17 15:46:25 +0000

    Update provider.tf
    Enable remote backend

--- provider.tf
+++ provider.tf
@@ -14,12 +14,12 @@ terraform {
   }  
 }
 
-# backend "remote" {
-# 	organization = "homelab-fsemti" # org name from step 2.
-# 	workspaces {
-# 		name = "homelab-fsemti" # name for your app's state.
-# 	}
-# }  
+ backend "remote" {
+ 	organization = "homelab-fsemti" # org name from step 2.
+ 	workspaces {
+ 		name = "homelab-fsemti" # name for your app's state.
+ 	}
+ }  
 
 
 provider "cloudflare" {
